612987bdfa8adb32a849a5a3db3a6203a300408e
SiEr0212 <simonertel@gmx.de>
2021-03-26 23:07:48 +0100

    added Style

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import logo from "./logo.svg";
 import "./App.scss";
 
 function App() {
-// eslint-disable-next-line no-console
-console.log('hi');
+  // eslint-disable-next-line no-console
+  console.log("hi");
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
+        <h1 className="name title">Names List</h1>
+        <h3 className="name">Peter</h3>
+        <h3 className="name">John</h3>
+        <h3 className="name">Jill</h3>
       </header>
     </div>
   );
