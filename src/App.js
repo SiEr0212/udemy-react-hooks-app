db08f35a8b0c1ea020f790238b2db096441fbeed
SiEr0212 <simonertel@gmx.de>
2021-03-26 23:31:33 +0100

    added nameTag component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.scss";
+import NameTag from "./components/nameTag";
 
 function App() {
   // eslint-disable-next-line no-console
@@ -9,9 +10,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 className="name title">Names List</h1>
-        <h3 className="name">Peter</h3>
-        <h3 className="name">John</h3>
-        <h3 className="name">Jill</h3>
+        <NameTag name="Peter"></NameTag>
+        <NameTag name="John"></NameTag>
+        <NameTag name="Jill"></NameTag>
       </header>
     </div>
   );
