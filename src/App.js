e24155b7939f17e745874ffcdc0f2c939061cf2b
SiEr0212 <simonertel@gmx.de>
2021-03-29 19:18:15 +0200

    added new Component Input.js

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 className="name title">Names List</h1>
-        <NameTag >Peter</NameTag>
-        <NameTag >John</NameTag>
-        <NameTag >Jill</NameTag>
+        <NameTag firstName="peter" lastName="johnson">Peter</NameTag>
+        <NameTag firstName="john" lastName="johnson">John</NameTag>
+        <NameTag firstName="jill" lastName="jillson">Jill</NameTag>
       </header>
     </div>
   );
