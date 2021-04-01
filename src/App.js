2a03cb006338f898fbc18b46247cefc50c4d0665
SiEr0212 <simonertel@gmx.de>
2021-04-01 21:12:15 +0200

    added age up and down functionality

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState} from 'react';
 import logo from "./logo.svg";
 import "./App.scss";
 import NameTag from "./components/nameTag";
@@ -29,12 +30,24 @@ const CleanNameTag = removeInline(NameTag);
 function App() {
   // eslint-disable-next-line no-console
   console.log("hi");
+  const [age, setAge] = useState(21);
+  const ageUpHandle = () => {
+    setAge(age + 1)
 
+  }
+  const ageDownHandle = () => {
+    setAge(age -1)
+  }
   return (
+   
     <div className="App">
       <header className="App-header">
         <Input placeholder="enter here" type="text"></Input>
         <h1 className="name title">Names List</h1>
+        <h2>Age: {age}</h2>
+        <button onClick={ageUpHandle}>Age up</button>
+        <button onClick={ageDownHandle}>Age down</button>
+        <br/>
         <GreenNameTag firstName="peter" lastName="johnson"></GreenNameTag>
         <CleanNameTag firstName="john" lastName="johnson"></CleanNameTag>
         <NameTag firstName="jill" lastName="jillson"></NameTag>
